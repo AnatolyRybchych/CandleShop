bc9c557448ecdc2338dd59bafa9a7fa6b16552d5
anatolyrybchych <tol.ryb@gmail.com>
2022-03-02 10:52:36 +0200

    edit database layout

--- .Database/layout.sql
+++ .Database/layout.sql
@@ -2,18 +2,16 @@ USE [Candle]
 
 CREATE TABLE LayoutHeader(
     Id INT PRIMARY KEY IDENTITY(1,1),
-    Region VARCHAR(50),
-    Short VARCHAR(4),
 
     Home NVARCHAR(15),
     About NVARCHAR(15),
     Shop NVARCHAR(15),
-
-    UNIQUE(Short),
-    UNIQUE(Region),
 );
 
-INSERT INTO LayoutHeader (Region, Short, Home, About, Shop) VALUES(
-    'Ukraine', 'UA', N'Головна', N'Про нас', N'Придбати'
+INSERT INTO LayoutHeader (Home, About, Shop) 
+VALUES(
+    N'Головна', N'Про нас', N'Придбати'
 );
+
+
 
