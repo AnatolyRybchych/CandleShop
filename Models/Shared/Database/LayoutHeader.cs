ee687a117366d80241d59f8a7f9e762fb69b4bac
anatolyrybchych <tol.ryb@gmail.com>
2022-03-02 04:31:39 +0200

    LayoutHeader model for database table LayoutHeader

---
+++ Models/Shared/Database/LayoutHeader.cs
@@ -0,0 +1,27 @@
+using CandleShop.Models.Shared;
+
+namespace CandleShop.Models.Shared.Database
+{
+    public class LayoutHeader : IDatabaseTable
+    {
+        public string TableName => "LayoutHeader";
+
+        [DatabaseClolumn("Id", DatabaseClolumnAttribute.DBColAttribs.Select, System.Data.SqlDbType.Int)]
+        public int Id{ get; set; }
+
+        [DatabaseClolumn("Region", DatabaseClolumnAttribute.DBColAttribs.All, System.Data.SqlDbType.VarChar)]
+        public string? Region{ get; set; }
+
+        [DatabaseClolumn("Short", DatabaseClolumnAttribute.DBColAttribs.All, System.Data.SqlDbType.VarChar)]
+        public string? Short{ get; set; }
+
+        [DatabaseClolumn("Home", DatabaseClolumnAttribute.DBColAttribs.All, System.Data.SqlDbType.VarChar)]
+        public string? Home{ get; set; }
+
+        [DatabaseClolumn("About", DatabaseClolumnAttribute.DBColAttribs.All, System.Data.SqlDbType.VarChar)]
+        public string? About{ get; set; }
+
+        [DatabaseClolumn("Shop", DatabaseClolumnAttribute.DBColAttribs.All, System.Data.SqlDbType.VarChar)]
+        public string? Shop{ get; set; }
+    }
+}
